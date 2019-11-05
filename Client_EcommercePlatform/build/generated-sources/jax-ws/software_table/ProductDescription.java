54f1492b4acba36e0b59bacbd4662613b6e0d1cd
Kak D-Wing <avehwing@gmail.com>
2019-11-05 14:16:19 +0800

    Create ProductDescription.java

---
+++ Client_EcommercePlatform/build/generated-sources/jax-ws/software_table/ProductDescription.java
@@ -0,0 +1,32 @@
+
+package software_table;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for product_description complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="product_description"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "product_description")
+public class ProductDescription {
+
+
+}
